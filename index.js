909bcfcd58ebe8ec4d1fba05243c57a23298b46c
tanmoypramanick420 <pramanickt15@gmail.com>
2025-11-20 18:05:57 +0530

    Update backend files

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 8001;
 connectToMongoDB('mongodb+srv://pramanickt15_db_user:Tanmoy123@cluster0.ggvgec6.mongodb.net/?appName=Cluster0').then(() => console.log('Mongodb connected')).catch((err) => console.log("DB Connection Error:", err));
 
 app.use(express.json())
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: "GET,POST,PUT,DELETE,PATCH",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 // Enable CORS for development (allow requests from frontend dev server)
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*');
